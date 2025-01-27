5aa198a5e4108b3c147fa4408891ec49799d2799
Joti <tihonjozsef@gmail.com>
2025-01-27 18:25:55 +0100

    Question text animation

--- src/main/webapp/resources/js/misc.js
+++ src/main/webapp/resources/js/misc.js
@@ -27,3 +27,87 @@ function addOutsideClickListener() {
 function removeOutsideClickListener() {
     $(document).off('click.outsideDialog');
 }
+
+function splitQuestionToLetters() {
+    const questionElement = document.getElementById('pageform:ot_question');
+    if (!questionElement) {
+        console.error('Element with id "ot_question" not found.');
+        return;
+    }
+
+    const text = questionElement.textContent.trim();
+    questionElement.textContent = ''; // Töröljük az eredeti tartalmat
+
+    // Szavakra bontás
+    const words = text.split(' ');
+
+    words.forEach((word, wordIndex) => {
+        // Szóhoz tartozó span létrehozása
+        const wordSpan = document.createElement('span');
+        wordSpan.classList.add('question-word'); // Szóhoz tartozó osztály hozzáadása
+
+        // Betűk hozzáadása a szóhoz
+        for (const letter of word) {
+            const letterSpan = document.createElement('span');
+            letterSpan.classList.add('question-letter'); // Betűhöz tartozó osztály hozzáadása
+            letterSpan.textContent = letter;
+            wordSpan.appendChild(letterSpan);
+        }
+
+        questionElement.appendChild(wordSpan);
+
+        // Ha nem az utolsó szó, szóközt is hozzáadunk
+        if (wordIndex < words.length - 1) {
+            questionElement.appendChild(document.createTextNode(' '));
+        }
+    });
+    
+    animateLetters();
+}
+
+function animateLetters() {
+
+  anime.timeline({loop: false})
+    .add({
+      targets: '.question-letter',
+      rotateY: [-90, 0],
+      duration: 7,
+      delay: (el, i) => 25 * i,
+      begin: function(anim) {
+        // Az animáció kezdésekor minden betűt láthatóvá teszünk
+        document.querySelectorAll('.question-letter').forEach(el => {
+          el.style.visibility = 'visible';
+        });
+      },
+      complete: function() {
+        // Az animáció végén az explanation elemet láthatóvá tesszük
+        const explanationElement = document.querySelector('.explanation');
+        if (explanationElement) {
+          explanationElement.style.visibility = 'visible';
+        }
+      }      
+      
+      
+    })
+    ;
+    
+}   
+
+function animateLettersDisappear() {
+  
+  const explanationElement = document.querySelector('.explanation');
+  if (explanationElement) {
+    explanationElement.style.visibility = 'hidden';
+  }
+
+  anime.timeline({loop: false})
+    .add({
+      targets: '.question-letter',
+      opacity: [1, 0],  // Az elem fokozatosan eltűnik
+      scale: [1, 0.5],  // Az elem összemegy, így jobban látható, hogy eltűnik
+      duration: 500,  // Az animáció időtartama
+      delay: (el, i) => 25 * i,  // Késleltetés, hogy a betűk egymás után tűnjenek el
+    });
+    
+}
+
